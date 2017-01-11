7e67693f0cf7e50f05cc7f56f0fc0b71996f1381
Yubin <yoobintse@gmail.com>
2017-01-11 16:46:48 -0600

    more readable

--- case-control.py
+++ case-control.py
@@ -1,19 +1,34 @@
 import vcf
 import subprocess
+#import sys, optparse
+
+######## This part can be comstumed###########
+#HeaderFile is a VCF file that contains your sample name information, no variants and sample information required#
+HeaderFile="../Files/example.vcf"
+#InputFileName is a VCF file that contains your sample name information, no variants and sample information required#
+InputFileName = "MIGen_ExS_28K.v6.ATVB_NoDup.vcf.gz"
+#CasePattern is the string feature that is only contained in case sample name# 
+CasePattern="A"
+#ControlPattern is the string feature that is only contained in control sample name# 
+ControlPattern="B"
 
 
-vcf_reader = vcf.Reader(open('../Files/example.vcf', 'r'))
+
+
+vcf_reader = vcf.Reader(open(HeaderFile, 'r'))
 #record = next(vcf_reader)
 SampleNameList=vcf_reader.samples
-Commend=""
-NumberPlus=0
-NumberMinus=0
-NumberZero=0
-Case=[]
-CaseString=""
+Commend = ""
+#To count the case sample number
+NumberPlus = 0
+#To count the control sample number
+NumberMinus = 0
+NumberZero = 0
+Case = []
+CaseString = ""
 Control=[]
 ControlString=""
-for sample in SampleNameList:
+for CasePattern in SampleNameList:
 	if "A" in sample:
 		Commend=Commend+"+"
 		Case.append(sample)
@@ -32,8 +47,8 @@ print NumberPlus, NumberMinus, NumberZero,Case,Control
 print CaseString
 print ControlString
 
-CaseCommendLine="bcftools view -s " + CaseString+ " MIGen_ExS_28K.v6.ATVB_NoDup.vcf.gz | bgzip -c > MIGen_ExS_28K.v6.ATVB_NoDup_Case.vcf.gz"
-ControlCommendLine="bcftools view -s " + ControlString+ " MIGen_ExS_28K.v6.ATVB_NoDup.vcf.gz | bgzip -c > MIGen_ExS_28K.v6.ATVB_NoDup_Control.vcf.gz"
+CaseCommendLine="bcftools view -s " + CaseString + " " + InputFileName + " | bgzip -c > " + InputFileName + "_Case.vcf.gz"
+ControlCommendLine="bcftools view -s " + ControlString + " " + InputFileName + " | bgzip -c > " + InputFileName + "_Control.vcf.gz"
 CommendLine= CaseCommendLine + " && " + ControlCommendLine
 
 subprocess.Popen(CommendLine,shell=True)
